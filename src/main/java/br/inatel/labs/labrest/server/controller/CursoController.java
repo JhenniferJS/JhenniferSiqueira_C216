582c239944192662b70facd0e016aaae18655113
JhenniferJS <jhennifer@gec.inatel.br>
2022-09-23 15:40:57 -0300

    adicionando testes

--- src/main/java/br/inatel/labs/labrest/server/controller/CursoController.java
+++ src/main/java/br/inatel/labs/labrest/server/controller/CursoController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import br.inatel.labs.labrest.server.exception.CursoNaoEncontradoException;
 import br.inatel.labs.labrest.server.model.Curso;
---
+++ src/test/java/br/inatel/labs/labrest/server/controller/CursoControllerTest.java
@@ -0,0 +1,119 @@
+package br.inatel.labs.labrest.server.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import br.inatel.labs.labrest.server.model.Curso;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class CursoControllerTest {
+	
+	@Autowired
+	private WebTestClient webTestClient;
+	
+	@Test
+	void deveListarCursos() {
+		webTestClient.get()
+		.uri("/curso")
+		.exchange()
+		.expectStatus()
+		.isOk()
+		.expectBody()
+		.returnResult();
+	}
+	
+	@Test
+	void dadoCursoIdValido_quandoGetCursoPeloId_entaoRespondeComCursoValido() {
+		Long cursoIdValido = 1L;
+		
+		Curso cursoRespondido = webTestClient.get()
+				.uri("/curso/" + cursoIdValido)
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.expectBody(Curso.class)
+				.returnResult()
+				.getResponseBody();
+		
+		assertNotNull(cursoRespondido);
+		assertEquals(cursoRespondido.getId(), cursoIdValido);
+	}
+	
+	@Test
+	void dadoCursoIdInvalido_quandoGetCursoPeloId_entaoRespondeComStatusNotFound() {
+		Long cursoIdInvalido = 99L;
+		
+		webTestClient.get()
+		.uri("/curso/" + cursoIdInvalido)
+		.exchange()
+		.expectStatus()
+		.isNotFound();
+	}
+
+	@Test
+	void dadoCursoValido_quandoPostCurso_entaoRespondeComCursoValido() {
+		Curso cursoValido = new Curso();
+		cursoValido.setDescricao("SpringBoot Test");
+		cursoValido.setCargaHoraria(80);
+		
+		Curso cursoRespondido = webTestClient
+				.post()
+				.uri("/curso")
+				.bodyValue(cursoValido)
+				.exchange()
+				.expectStatus()
+				.isCreated()
+				.expectBody(Curso.class)
+				.returnResult()
+				.getResponseBody();
+
+		assertNotNull(cursoRespondido.getId());
+		assertEquals(cursoRespondido.getDescricao(), cursoValido.getDescricao());
+		assertEquals(cursoRespondido.getCargaHoraria(), cursoValido.getCargaHoraria());
+	}
+	
+	@Test
+	void dadoCursoIdValido_quandoPutCurso_entaoRespondeComStatusAccept() {
+		Curso cursoValido = new Curso();
+		cursoValido.setDescricao("SpringBoot Test");
+		cursoValido.setCargaHoraria(80);
+		
+		Long cursoIdExistente = 2L;
+		
+		webTestClient.put()
+		.uri("/curso/" + cursoIdExistente)
+		.bodyValue(cursoValido)
+		.exchange()
+		.expectStatus()
+		.isAccepted();
+	}
+	
+	@Test
+	void dadoCursoIdValido_quandoDeleteCurso_entaoRespondeComStatusNoContent() {
+		Long cursoIdValido = 3L;
+		
+		webTestClient.delete()
+		.uri("/curso/" + cursoIdValido)
+		.exchange()
+		.expectStatus()
+		.isNoContent();
+	}
+	
+	@Test
+	void dadoCursoIdInvalido_quandoDeleteCurso_entaoRespondeComStatusNotFound() {
+		Long cursoIdInvalido = 99L;
+		
+		webTestClient.delete()
+		.uri("/curso/" + cursoIdInvalido)
+		.exchange()
+		.expectStatus()
+		.isNotFound();
+	}
+}
