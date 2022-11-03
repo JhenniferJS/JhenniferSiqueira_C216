cb6ad33cac7fd986da38435ef4059de89c6fe781
JhenniferJS <jhennifer@gec.inatel.br>
2022-11-03 18:26:51 -0300

    terceiro lab de jpa

---
+++ src/test/java/br/inatel/labs/labjpa/LoadingDemo.java
@@ -0,0 +1,29 @@
+package br.inatel.labs.labjpa;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.inatel.labs.labjpa.entity.NotaCompraItem;
+import br.inatel.labs.labjpa.service.NotaCompraService;
+
+@SpringBootTest
+public class LoadingDemo {
+	
+	@Autowired
+	NotaCompraService service;
+	
+	public void demoEagerLoading() {
+		try {
+			NotaCompraItem item = service.buscarNotaCompraItemPeloId(1L);
+			LocalDate dataEmissao = item.getNotaCompra().getDataEmissao();
+			
+			System.out.println(dataEmissao);
+			System.out.println("Aconteceu carregamento EAGER");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
